75b35f882893b1a394ebfb695ce47b108a875d0d
배은영 <adcadcad@naver.com>
2026-06-26 18:34:03 +0900

    fix: move dotenv config function before import app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
-import app from './app';
 import dotenv from 'dotenv';
-
 dotenv.config()
+
+import app from './app';
+
 
 // 환경변수에 PORT 값이 설정되어 있지 않으면 기본값으로 4000번 포트를 사용
 const PORT = process.env.PORT || 4000; 
