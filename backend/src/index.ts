aa2e8aac9c6e1fda1956d2f02351548640284ee5
배은영 <adcadcad@naver.com>
2026-06-26 18:15:26 +0900

    feat: setup Express.js TS backend server

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import app from './app';
+
+dotenv.config()
+
+// 환경변수에 PORT 값이 설정되어 있지 않으면 기본값으로 4000번 포트를 사용
+const PORT = process.env.PORT || 4000; 
+
+// 서버 시작
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
