346683d0691eabdb0f759108ca72faddb3278067
배은영 <adcadcad@naver.com>
2026-07-08 16:07:15 +0900

    refactor: 라우트 설정 분리 및 React.lazy 코드 스플리팅 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,12 @@
-import {Routes, Route, Navigate} from 'react-router-dom'
 import './App.css'
-import PublicRoute from './components/PublicRoute'
-import PrivateRoute from './components/PrivateRoute'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
 import AuthProvider from './providers/AuthProvider'
-import BirthProfileFormPage from './pages/BirthProfileFormPage'
-import ResultPage from './pages/ResultPage'
-import FortunePage from './pages/FortunePage'
-import ArchivePage from './pages/ArchivePage'
-import SettingsPage from './pages/SettingsPage'
+import AppRouter from './router'
 
 
 function App() {
-
   return (
     <AuthProvider>
-      <Routes>
-        {/* 비로그인/로그인 모두 접근 가능 */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/saju/new" element={<BirthProfileFormPage />} />
-        <Route path="/result" element={<ResultPage />} />
-
-        {/* 로그인 상태에서 접근 시 홈으로 */}
-        <Route element={<PublicRoute />}>
-          <Route path="/login" element={<LoginPage />} />
-        </Route>
-
-        {/* 로그인 필요 */}
-        <Route element={<PrivateRoute />}>
-          <Route path="/fortune" element={<FortunePage />} />
-          <Route path="/archive" element={<ArchivePage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/login" replace />} />
-     </Routes>
+      <AppRouter />
     </AuthProvider>
   )
 }
